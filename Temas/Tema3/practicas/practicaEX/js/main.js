569d1a61a37f7bdae51785fd5441094ba7511a45
rubhermar96 <rubhermar96@gmail.com>
2021-02-04 19:17:24 +0100

    asd

---
+++ Temas/Tema3/practicas/practicaEX/js/main.js
@@ -0,0 +1,18 @@
+function startJS(){
+    $(function(){
+        //Abrir Menu Desplegable
+        $("#openMenu").click(function(){
+          $(".menu-desplegable").css("display","flex");
+          $(".menu-desplegable").animate({width:"100%"},1000);
+          $("#openMenu").css("visibility","hidden");
+        });
+        //Cerrar Menu Desplegable
+        $("#closeMenu").click(function(){
+          $(".menu-desplegable").animate({width:"0%"},0);
+          $("#openMenu").css("visibility","visible");
+          $(".menu-desplegable").css("display","none");
+          
+        });
+    });
+};
+window.onload=startJS;
